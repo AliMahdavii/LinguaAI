73f59f9bb2a2af4bd1d283d99a82675aee66cfc6
Ali <84a.mahdaviniya@gmail.com>
2026-09-13 17:33:42 +0330

    feat: connect telegram bot

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
-print("LinguaAI is starting...")
+import os
+
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(
+        message,
+        "Hi"
+    )
+
+
+def main():
+    print("LinguaAI is running...")
+    bot.infinity_polling()
+
+
+if __name__ == "__main__":
+    main()
